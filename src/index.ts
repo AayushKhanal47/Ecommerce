659748935be9d4d2cf311bf905e3ba25ccdbb0ae
AayushKhanal47 <aayushkhanal810@gmail.com>
2025-04-11 01:22:55 +0530

    A

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express, {Express, Request, Response} from "express";
+import {PORT} from './secrets'
+import rootRouter from "./routes";
+
+const app = express();
+
+app.use('/api', rootRouter)
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
