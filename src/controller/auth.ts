9fcaa45835653deb39beaa41a7736fc4d25a8a7d
AayushKhanal47 <aayushkhanal810@gmail.com>
2025-04-11 15:14:10 +0530

    login and signup

--- src/controller/auth.ts
+++ src/controller/auth.ts
@@ -1,6 +1,8 @@
 import {Request ,Response} from 'express'
-import {hashSync} from 'bcrypt'
+import {hashSync, compareSync} from 'bcrypt'
 import { prismaClient } from '..'
+import * as jwt from 'jsonwebtoken'
+import { JWT_SECRET } from '../secrets'; 
 
 
 export const signup = async (req:Request, res:Response)=>{
@@ -17,5 +19,19 @@ export const signup = async (req:Request, res:Response)=>{
         }
     })
     res.json(user)
+}
+
+export const login = async (req:Request, res:Response)=>{
+    const {email, password } = req.body;
+    let user = await prismaClient.user.findFirst({where:{email}})
+    if (!user){  throw Error('User not exists')
+          }
+        if(!compareSync(password, user.password)){
+            throw Error('Incorrect Password')
+        }
+        const token = jwt.sign({
+            userId:user.id
+        },JWT_SECRET)
+        res.json({ user, token });
 }
 
